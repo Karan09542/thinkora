15ab45ae8fdc4048c76827f47649ecb18ceb4ede
Karan09542 <karan09542@gmail.com>
2026-01-19 20:47:01 +0530

    correct vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react(), tailwindcss()],
